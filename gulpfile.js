2e0785c6cdeac365cda60cdff69d09930e1accb4
Niko Chaffin <niko@bixly.com>
2016-12-29 14:56:48 -0800

    Updated gulpfile, added dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var gutil = require('gulp-util');
+var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var colors = require('colors');
+var named = require('vinyl-named');
 
 var sassPath = './src/css/**/*.scss';
-var cssOutput = './src/css';
+var jsPath = './src/js/**/*.js';
+var cssOutput = './dist/css';
+var jsOutput = './dist/js';
+
+gulp.task('bundle-js', function() {
+  var w = webpack({
+    output: {
+      filename: '[name].js'
+    }
+  });
+  w.on('error', function(e) {
+    this.emit('end');
+  });
+  return gulp.src('src/js/*.js')
+    .pipe(named())
+    .pipe(w)
+    .pipe(gulp.dest(jsOutput))
+    .pipe(uglify())
+    .pipe(rename(function(path) {
+      path.basename += ".min"
+    }))
+    .pipe(gulp.dest(jsOutput))
+});
 
 // Compile SCSS files
 gulp.task('compile-sass', function() {
@@ -30,7 +55,8 @@ gulp.task('compile-sass', function() {
 });
 
 gulp.task('watch', function() {
-  return gulp.watch(sassPath, ['compile-sass']);
+  gulp.watch(sassPath, ['compile-sass']);
+  return gulp.watch(jsPath, ['bundle-js']);
 });
 
 // Django server and Browser Sync
---
+++ src/js/typemonics.js
@@ -0,0 +1,3 @@
+(function() {
+  console.log("Rocket science. 🚀");
+}());
