511d8c439063392d28c77b8e79ca1f2d3e822452
Niko Chaffin <niko@bixly.com>
2016-12-29 16:51:17 -0800

    Added input for adjusting base-line-height

--- dist/js/typemonics.js
+++ dist/js/typemonics.js
@@ -73,6 +73,7 @@
 
 	  var data = {};
 	  var allEls = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+	  var minLineHeightMultiple = 0.5;
 
 	  var inputs = document.querySelectorAll('.control-panel input');
 	  for (var i = 0; i < inputs.length; i++) {
@@ -84,10 +85,18 @@
 	  function onInputChange(e) {
 	    var name = e.target.name;
 	    var val = floatElseString(e.target.value);
+	    if (typeof val === "string" && e.target.hasAttribute('data-val-min')) {
+	      var _min = parseFloat(e.target.getAttribute('data-val-min'));
+	      var _val = splitUnits(val);
+	      if (_val.val < _min) {
+	        return;
+	      }
+	    }
 
 	    data[name] = val;
 
-	    if (name == "font-base" || name == "ratio") {
+	    var updateAllKeys = ['font-base', 'ratio', 'base-line-height'];
+	    if (updateAllKeys.indexOf(name) > -1) {
 	      updateAllFontSizes(data);
 	    } else if (name.indexOf('-step') > -1) {
 	      var el = name.replace('-step', '');
@@ -104,11 +113,23 @@
 	  function updateStyling(el, data) {
 	    var els = document.querySelectorAll('.sample-content ' + el);
 	    var size = getFontSize(el);
+	    var lh = getLineHeight(size, data, minLineHeightMultiple);
+	    console.log(el, size, lh);
 
 	    for (var i = 0; i < els.length; i++) {
-	      console.log(size);
 	      els[i].style.fontSize = size;
+	      els[i].style.lineHeight = lh;
 	    }
+	  }
+
+	  function getLineHeight(fontSize, data, minLineHeightMultiple) {
+	    var _lh = splitUnits(data['base-line-height']);
+	    var out = _lh.val;
+	    var _fs = splitUnits(fontSize);
+	    while (out < _fs.val) {
+	      out += _lh.val * minLineHeightMultiple;
+	    }
+	    return out + _lh.units;
 	  }
 
 	  function getFontSize(el) {
@@ -117,7 +138,6 @@
 	    var step = data[el + '-step'];
 	    var size;
 	    if (step !== undefined) {
-	      console.log(step, base.val, ratio);
 	      size = (ms.ms(step, [base.val], [ratio]) * base.val) + base.units;
 	    } else {
 	      size = data['font-base'];
